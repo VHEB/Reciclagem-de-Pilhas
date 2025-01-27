ac11c00b450bcbd64dfe1b4e42c362b023fcceca
VHEB <vheb0316@gmail.com>
2025-01-26 22:20:11 -0300

    feat: Implementação de acessibilidade +-

--- ReciclaPilhas/core/static/javascript/acessibilidade.js
+++ ReciclaPilhas/core/static/javascript/acessibilidade.js
@@ -1,42 +1,42 @@
+window.addEventListener('load', () => {
+    const aumentarFonte = document.getElementById('aumentar-fonte');
+    const diminuirFonte = document.getElementById('diminuir-fonte');
+    const normalizarFonte = document.getElementById('normalizar-fonte');
+    const contraste = document.getElementById('contraste');
+    //const vlibrasButton = document.getElementById('vlibras-button');  // Certifique-se de que esta linha está presente
 
+    let fontSize = 1;
 
-document.addEventListener('DOMContentLoaded', () => {
-    const aumentarFonte = document.getElementById('aumentar-fonte')
-    const diminuirFonte = document.getElementById('diminuir-fonte')
-    const normalizarFonte = document.getElementById('normalizar-fonte')
-    const contraste = document.getElementById('contraste')
-
-    let fontSize = 1
-
-    aumentarFonte.addEventListener('click', function () {
-        if (fontSize <= 1.4) {
-            fontSize += 0.1
-            document.body.style.fontSize = fontSize + 'em'
-        }
-    })
-
-    diminuirFonte.addEventListener('click', function () {
-        if (fontSize > 0.8) {
-            fontSize -= 0.1
-            document.body.style.fontSize = fontSize + 'em'
-        }
-    })
-
-    normalizarFonte.addEventListener('click', function () {
-        fontSize = 1
-        document.body.style.fontSize = '1em'
-    })
-
-    contraste.addEventListener('click', function () {
-        document.body.classList.toggle('alto-contraste')
-    })
-})
-
-document.getElementById('vlibras-button').addEventListener('click', function () {
-    const vlibrasWidget = document.querySelector('div[vw-access-button]');
-    if (vlibrasWidget) {
-        vlibrasWidget.click();
-    } else {
-        alert("VLibras ainda não carregou. Tente novamente em alguns segundos.");
+    if (aumentarFonte) {
+        aumentarFonte.addEventListener('click', function () {
+            if (fontSize <= 1.4) {
+                fontSize += 0.1;
+                document.body.style.fontSize = fontSize + 'em';
+            }
+        });
     }
+
+    if (diminuirFonte) {
+        diminuirFonte.addEventListener('click', function () {
+            if (fontSize > 0.8) {
+                fontSize -= 0.1;
+                document.body.style.fontSize = fontSize + 'em';
+            }
+        });
+    }
+
+    if (normalizarFonte) {
+        normalizarFonte.addEventListener('click', function () {
+            fontSize = 1;
+            document.body.style.fontSize = '1em';
+        });
+    }
+
+    if (contraste) {
+        contraste.addEventListener('click', function () {
+            document.body.classList.toggle('alto-contraste');
+        });
+    }
+
+    
 });
